d146dc7f68a7a8b16c2c2ff8693ac45de9008202
kea20055 <81277449+kea20055@users.noreply.github.com>
2021-07-31 22:00:24 -0400

    Add files via upload

---
+++ js-2/music-list.js
@@ -0,0 +1,105 @@
+let allMusic = [
+    {
+      name: "Save your tears",
+      artist: "The Weeknd",
+      img: "save your tears",
+      src: "save your tears"
+    },
+    {
+      name: "Blinding Lights",
+      artist: "The Weeknd",
+      img: "blinding lights",
+      src: "blinding lights"
+    },
+    {
+      name: "The hills",
+      artist: "The Weeknd",
+      img: "the hills",
+      src: "the hills"
+    },
+    {
+      name: "Starboy",
+      artist: "The Weeknd",
+      img: "starboy",
+      src: "starboy"
+    },
+    {
+      name: "I fell it coming",
+      artist: "The Weeknd",
+      img: "i fell it coming",
+      src: "i fell it coming"
+    },
+    {
+      name: "Call out my name",
+      artist: "The Weeknd",
+      img: "call out my name",
+      src: "call out my name"
+    },
+    {
+      name: "Die for you",
+      artist: "The Weeknd",
+      img: "die for you",
+      src: "die for you"
+    },
+    {
+    name: "Can´t fell my face",
+    artist: "The Weeknd",
+    img: "cant fell my face",
+    src: "cant fell my face"
+    },
+    {
+    name: "After hours",
+    artist: "The Weeknd",
+    img: "after hours",
+    src: "after hours"
+    },
+    {
+    name: "In your eyes",
+    artist: "The Weeknd",
+    img: "in your eyes",
+    src: "in your eyes"
+    },
+    {
+    name: "Secrets",
+    artist: "The Weeknd",
+    img: "secrets",
+    src: "secrets"
+    },
+    {
+    name: "Earned it",
+    artist: "The Weeknd",
+    img: "earned it",
+    src: "earned it"
+    },
+    {
+    name: "Snowchild",
+    artist: "The Weeknd",
+    img: "snowchild",
+    src: "snowchild"
+    },
+    {
+    name: "In the night",
+    artist: "The Weeknd",
+    img: "in the night",
+    src: "in the night"
+    },
+    {
+    name: "Heartless",
+    artist: "The Weeknd",
+    img: "heartless",
+    src: "heartless"
+},
+{
+    name: "Until i bleed out",
+    artist: "The Weeknd",
+    img: "until i bleed out",
+    src: "until i bleed out"
+},
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
